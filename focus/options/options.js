93212f3c25e322297c3ef0fdd3017752a4282e6f
Michael Jenson <cl0n3dxm0nk3y+github@gmail.com>
2023-10-21 21:19:12 -0700

    Add and remove buttons now work for allowed and blocked URLs! For #2

--- focus/options/options.js
+++ focus/options/options.js
@@ -5,8 +5,12 @@
 //
   $('.allowlist_add').on('click', allowlist_add);
   $('.allowlist_remove').on('click', allowlist_remove);
+  $('.blocklist_add').on('click', blocklist_add);
+  $('.blocklist_remove').on('click', blocklist_remove);
 
-//const blockedURLsTextArea = document.querySelector("#blocked-urls");
+const allowedURLsInputs = document.querySelectorAll("#allowed_url_input");
+const blockedURLsInputs = document.querySelectorAll("#blocked_url_input");
+
 //
 //// Store the currently selected settings using browser.storage.local.
 //function storeSettings() {
@@ -34,15 +38,35 @@
 //
 //
 function allowlist_add() {
-//  var new_chq_no = parseInt($('#total_chq').val()) + 1;
+  let new_allowed_url_no = parseInt($('#total_allowed_urls').val()) + 1;
+  let new_allowed_url_input = "<div class='url_input' id='allow_url_input_" + new_allowed_url_no + "'><select id='protocol' name='protocol'><option value='*:' selected='selected'>http/https</option><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
   
-  var new_url_input = "<div class='url_input' id='allow_url_input_1'><select id='protocol' name='protocol'><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option><option value='*:'>*</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host' value='www.example.com'><label for='host'> / </label><input type='text' id='path' name='path' value='example/index.html'></div>"
-  //var new_url_input = "<input type='text' id='path' name='path' value='example/index.html'>"
-  
-  $('#allow_url_input_1').append(new_url_input)
-  
+  $('#allow_url_input_1').append(new_allowed_url_input);
+  $('#total_allowed_urls').val(new_allowed_url_no);
 }
 
 function allowlist_remove() {
-  var new_url_input = "<div class='url_input' id='allow_url_input_1'><select id='protocol' name='protocol'><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option><option value='*:'>*</option></select><label for='protocol'>://</label><input type='text' id='host' name='host' value='www.example.com'><label for='host'>/</label><input type='text' id='path' name='path' value='example/index.html'></div>"
+  let last_allowed_url_no = $('#total_allowed_urls').val();
+  
+  if (last_allowed_url_no > 1) {
+    $('#allow_url_input_' + last_allowed_url_no).remove();
+	$('#total_allowed_urls').val(last_allowed_url_no - 1);
+  }
+}
+
+function blocklist_add() {
+  let new_blocked_url_no = parseInt($('#total_blocked_urls').val()) + 1;
+  let new_blocked_url_input = "<div class='url_input' id='block_url_input_" + new_blocked_url_no + "'><select id='protocol' name='protocol'><option value='*:' selected='selected'>http/https</option><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
+  
+  $('#block_url_input_1').append(new_blocked_url_input);
+  $('#total_blocked_urls').val(new_blocked_url_no);
+}
+
+function blocklist_remove() {
+  let last_blocked_url_no = $('#total_blocked_urls').val();
+  
+  if (last_blocked_url_no > 1) {
+    $('#block_url_input_' + last_blocked_url_no).remove();
+	$('#total_blocked_urls').val(last_blocked_url_no - 1);
+  }
 }
