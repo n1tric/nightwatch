5f5553797d59d284d7344c87c3d5255ac8a970ae
Michael Jenson <mike@majenson.com>
2023-11-10 13:21:44 -0800

    Implemented new options page with block list AND allow list! Fixes #4

--- focus/background/focus.js
+++ focus/background/focus.js
@@ -1,5 +1,6 @@
 // Initialize the list of blocked hosts
-let blockedURLs = ["www.example.com/example","www.example.org/examples/example"];
+let blockedURLs = ["https://www.reddit.com/","https://www.example.com:654/example","http://www.example.org:9876/examples/index.html"];
+let allowedURLs = ["http://www.example.gov/ex/index.htm","https://www.example.com/x/12345"]
 //let blockedURLs = ["www.reddit.com/r/news/","www.reddit.com/r/worldnews/","www.reddit.com/"]//, "www.mozilla.org/en-US/"]//, "http://www.mozilla.org/en-US/"]//, "https://www.mozilla.org/en-US/"];
 
 // Set the default list on installation.
@@ -9,16 +10,31 @@ browser.runtime.onInstalled.addListener(details => {
   });
 });
 
-// Get the stored list
+browser.runtime.onInstalled.addListener(details => {
+  browser.storage.local.set({
+	allowedURLs: allowedURLs
+  });
+});
+// Get the stored lists
 browser.storage.local.get(data => {
   if (data.blockedURLs) {
     blockedURLs = data.blockedURLs;
+ }
+});
+
+browser.storage.local.get(data => {
+  if (data.allowedURLs) {
+    allowedURLs = data.allowedURLs;
   }
 });
 
 // Listen for changes in the blocked list
 browser.storage.onChanged.addListener(changeData => {
-  blockedURLs = changeData.blockedURLs.newValue;
+  blockedURLs = changeData.blockedURLs.newValue
+});
+
+browser.storage.onChanged.addListener(changeData => {
+  allowedURLs = changeData.allowedURLs.newValue
 });
 
 // Managed the proxy
@@ -28,18 +44,46 @@ browser.proxy.onRequest.addListener(handleProxyRequest, {urls: ["<all_urls>"]});
 
 // On the request to open a webpage
 function handleProxyRequest(requestInfo) {
-// Read the web address of the page to be visited 
+  // Read the web address of the page to be visited 
   const url = new URL(requestInfo.url);
-  console.log(`${url.host} + ${url.pathname}`);
-  console.log(`${url.host}`);
-// Determine whether the domain in the web address is on the blocked hosts list
-  if (blockedURLs.indexOf(url.host + url.pathname) != -1) {
-// Write details of the proxied host to the console and return the proxy address
-    console.log(`Proxying: ${url.host + url.pathname}`);
+  let escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  let allow = false;
+  let block = false;
+
+  // Determine whether the domain in the web address is on the allowed list
+  allowedURLs.forEach(function(aURL) {
+    allowedURL = new RegExp("^" + aURL.split("*").map(escapeRegex).join(".*") + "$");
+	if (allowedURL.test(url.protocol + "//" + url.host + url.pathname)) {
+	  console.log(`Allowing: ${url.protocol + "//" + url.host + url.pathname}`);
+	  allow = true;
+	}
+  })
+  
+  // Determine whether the domain in the web address is on the blocked list
+  blockedURLs.forEach(function(bURL) {
+    blockedURL = new RegExp("^" + bURL.split("*").map(escapeRegex).join(".*") + "$");
+	if (blockedURL.test(url.protocol + "//" + url.host + url.pathname)) {
+	  console.log(`Blocking: ${url.protocol + "//" + url.host + url.pathname}`);
+	  block = true;
+	}
+  })
+  //console.log(`After block`);
+//  if (blockedURLs.indexOf(url.protocol + "//" + url.host + url.pathname) != -1) {
+//  // Write details of the proxied host to the console and return the proxy address
+//    console.log(`Proxying: ${url.protocol + "//" + url.host + url.pathname}`);
+//    return {type: "http", host: "127.0.0.1", port: 65535};
+//  }
+
+  // Return instructions to open the requested webpage
+  if (allow) {
+    return {type: "direct"};
+  }
+  else if (block) {
     return {type: "http", host: "127.0.0.1", port: 65535};
   }
-// Return instructions to open the requested webpage
-  return {type: "direct"};
+  else {
+    return {type: "direct"};
+  }
 }
 
 // Log any errors from the proxy script
--- focus/options/options.js
+++ focus/options/options.js
@@ -8,47 +8,27 @@
   $('.blocklist_add').on('click', blocklist_add);
   $('.blocklist_remove').on('click', blocklist_remove);
 
-const allowedURLsInputs = document.querySelectorAll("#allowed_url_input");
-const blockedURLsInputs = document.querySelectorAll("#blocked_url_input");
+const allowedURLsInputs = document.querySelectorAll("div.allowed_url_input");
+const blockedURLsInputs = document.querySelectorAll("div.blocked_url_input");
 
-//
-//// Store the currently selected settings using browser.storage.local.
-//function storeSettings() {
-//  let blockedURLs = blockedURLsTextArea.value.split("\n");
-//  browser.storage.local.set({
-//    blockedURLs
-//  });
-//}
-//
-//// Update the options UI with the settings values retrieved from storage,
-//// or the default settings if the stored settings are empty.
-//function updateUI(restoredSettings) {
-//  blockedURLsTextArea.value = restoredSettings.blockedURLs.join("\n");
-//}
-//
-//function onError(e) {
-//  console.error(e);
-//}
-//
-//// On opening the options page, fetch stored settings and update the UI with them.
-//browser.storage.local.get().then(updateUI, onError);
-//
-//// Whenever the contents of the textarea changes, save the new values
-//blockedURLsTextArea.addEventListener("change", storeSettings);
-//
-//
+
 function allowlist_add() {
   let new_allowed_url_no = parseInt($('#total_allowed_urls').val()) + 1;
-  let new_allowed_url_input = "<div class='url_input' id='allow_url_input_" + new_allowed_url_no + "'><select id='protocol' name='protocol'><option value='*:' selected='selected'>http/https</option><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
+  let new_allowed_url_input = "<div class='url_input' id='allow_url_input_" + new_allowed_url_no + "'><select id='protocol' name='protocol'><option value='https:' selected='selected'>https</option><option value='http:'>http</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
   
-  $('#allow_url_input_1').append(new_allowed_url_input);
+  $('#allow_url_input_0').append(new_allowed_url_input);
   $('#total_allowed_urls').val(new_allowed_url_no);
+  
+  let i = allowedURLsInputs[0].childNodes.length -1;
+  allowedURLsInputs[0].childNodes[i].childNodes[0].addEventListener("change", storeAllowedSettings);
+  allowedURLsInputs[0].childNodes[i].childNodes[2].addEventListener("change", storeAllowedSettings);
+  allowedURLsInputs[0].childNodes[i].childNodes[4].addEventListener("change", storeAllowedSettings);
 }
 
 function allowlist_remove() {
   let last_allowed_url_no = $('#total_allowed_urls').val();
   
-  if (last_allowed_url_no > 1) {
+  if (last_allowed_url_no > 0) {
     $('#allow_url_input_' + last_allowed_url_no).remove();
 	$('#total_allowed_urls').val(last_allowed_url_no - 1);
   }
@@ -56,17 +36,80 @@ function allowlist_remove() {
 
 function blocklist_add() {
   let new_blocked_url_no = parseInt($('#total_blocked_urls').val()) + 1;
-  let new_blocked_url_input = "<div class='url_input' id='block_url_input_" + new_blocked_url_no + "'><select id='protocol' name='protocol'><option value='*:' selected='selected'>http/https</option><option value='https:'>https</option><option value='http:'>http</option><option value='file:'>file</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
+  let new_blocked_url_input = "<div class='url_input' id='block_url_input_" + new_blocked_url_no + "'><select id='protocol' name='protocol'><option value='https:' selected='selected'>https</option><option value='http:'>http</option></select><label for='protocol'> :// </label><input type='text' id='host' name='host'><label for='host'> / </label><input type='text' id='path' name='path'></div>";
   
-  $('#block_url_input_1').append(new_blocked_url_input);
+  $('#block_url_input_0').append(new_blocked_url_input);
   $('#total_blocked_urls').val(new_blocked_url_no);
+  
+  let i = blockedURLsInputs[0].childNodes.length -1;
+  blockedURLsInputs[0].childNodes[i].childNodes[0].addEventListener("change", storeBlockedSettings);
+  blockedURLsInputs[0].childNodes[i].childNodes[2].addEventListener("change", storeBlockedSettings);
+  blockedURLsInputs[0].childNodes[i].childNodes[4].addEventListener("change", storeBlockedSettings);
 }
 
 function blocklist_remove() {
   let last_blocked_url_no = $('#total_blocked_urls').val();
   
-  if (last_blocked_url_no > 1) {
+  if (last_blocked_url_no > 0) {
     $('#block_url_input_' + last_blocked_url_no).remove();
 	$('#total_blocked_urls').val(last_blocked_url_no - 1);
   }
 }
+
+
+// Store the currently selected settings using browser.storage.local.
+function storeAllowedSettings() {
+  let allowedURLs = [];
+  
+  for (let i = 0; i < allowedURLsInputs[0].childNodes.length; i++) {
+	  allowedURLs.push(allowedURLsInputs[0].childNodes[i].childNodes[0].value + "//" + allowedURLsInputs[0].childNodes[i].childNodes[2].value + "/" + allowedURLsInputs[0].childNodes[i].childNodes[4].value);
+  }
+  
+  browser.storage.local.set({
+	allowedURLs
+  });
+}
+
+function storeBlockedSettings() {
+  let blockedURLs = [];
+  
+  for (let i = 0; i < blockedURLsInputs[0].childNodes.length; i++) {
+	  blockedURLs.push(blockedURLsInputs[0].childNodes[i].childNodes[0].value + "//" + blockedURLsInputs[0].childNodes[i].childNodes[2].value + "/" + blockedURLsInputs[0].childNodes[i].childNodes[4].value);
+  }
+  
+  browser.storage.local.set({
+    blockedURLs
+  });
+}
+
+// Update the options UI with the settings values retrieved from storage,
+// or the default settings if the stored settings are empty.
+function updateUI(restoredSettings) {
+  restoredSettings.allowedURLs.forEach(function(aURL){
+    allowlist_add();
+    allowedURL = new URL(aURL);
+    let i = allowedURLsInputs[0].childNodes.length - 1;
+    allowedURLsInputs[0].childNodes[i].childNodes[0].value = allowedURL.protocol;
+    allowedURLsInputs[0].childNodes[i].childNodes[2].value = allowedURL.host;
+    allowedURLsInputs[0].childNodes[i].childNodes[4].value = allowedURL.pathname.slice(1);
+  });
+  restoredSettings.blockedURLs.forEach(function(bURL){
+    blocklist_add();
+	blockedURL = new URL(bURL);
+	let i = blockedURLsInputs[0].childNodes.length - 1;
+    blockedURLsInputs[0].childNodes[i].childNodes[0].value = blockedURL.protocol;
+    blockedURLsInputs[0].childNodes[i].childNodes[2].value = blockedURL.host;
+    blockedURLsInputs[0].childNodes[i].childNodes[4].value = blockedURL.pathname.slice(1);
+  });
+}
+
+function onError(e) {
+  console.error(e);
+}
+
+// On opening the options page, fetch stored settings and update the UI with them.
+browser.storage.local.get().then(updateUI, onError);
+
+// Whenever the contents of the textarea changes, save the new values
+//allowedURLsInputs.addEventListener("change", storeAllowedSettings);
+//blockedURLsInputs.addEventListener("change", storeBlockedSettings);
